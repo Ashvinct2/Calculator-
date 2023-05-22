402a6739785380167be4a7d14eaf8eb1de479b15
Ashvinct2 <133523688+Ashvinct2@users.noreply.github.com>
2023-05-22 20:54:38 +0530

    javascript.java

--- javascript.java
+++ javascript.java
@@ -1,11 +1,21 @@
 var screen = document.getElementById('screen');
+
 function buttonclick(value){
+
     screen.value +=value;
+
 }
+
 function  clearscreen(){
+
     screen.value = "";
+
 }
+
 function findresult(){
+
     var result = eval(screen.value)
+
     screen.value = result;
+
 }
